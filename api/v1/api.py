5bbc2c0f9c7bbc4d1d28553af351f74729156303
Hugo Nicolau <huugoniicolau@gmail.com>
2023-06-02 11:58:24 -0300

    Modificacao da tag Funcionario_fabrica

--- api/v1/api.py
+++ api/v1/api.py
@@ -4,5 +4,5 @@ from api.v1.endpoints import funcionario_fabrica
 from api.v1.endpoints import funcionario
 
 api_router = APIRouter()
-api_router.include_router(funcionario_fabrica.router, prefix='/funcionario_fabrica',tags=["funcionario_fabrica"])
+api_router.include_router(funcionario_fabrica.router, prefix='/funcionario_fabrica',tags=["Funcionario_Fabrica"])
 api_router.include_router(funcionario.router,prefix='/funcionario',tags=["Funcionario"])
