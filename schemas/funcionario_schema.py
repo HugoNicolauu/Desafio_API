e12da6afe138df7f533a86fee63d3e4bdfac28cc
Hugo Nicolau <huugoniicolau@gmail.com>
2023-06-02 11:40:15 -0300

    import do date em datetime

--- schemas/funcionario_schema.py
+++ schemas/funcionario_schema.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, date
 
 class FuncionarioSchema(BaseModel):
     id: Optional[int]
