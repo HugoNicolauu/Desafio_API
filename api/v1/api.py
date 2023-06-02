432150a9c9a123750f2269be6b58714c8494926f
Hugo Nicolau <huugoniicolau@gmail.com>
2023-06-02 11:39:41 -0300

    inclusao da rota funcionario em api

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints import funcionario_fabrica
+from api.v1.endpoints import funcionario
 
 api_router = APIRouter()
 api_router.include_router(funcionario_fabrica.router, prefix='/funcionario_fabrica',tags=["funcionario_fabrica"])
+api_router.include_router(funcionario.router,prefix='/funcionario',tags=["Funcionario"])
